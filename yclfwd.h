b9b67060cdedfb90035795458c26b88401c4642c
yuldk <yul.dev.kim@gmail.com>
2021-12-17 00:39:33 +0900

    #0005 add foward header
    - add foward header

---
+++ yclfwd.h
@@ -0,0 +1,15 @@
+// *----------------------------------------------------------------------------------------------
+// *  Copyright (c) yuldk. All rights reserved.
+// *  Licensed under the MIT License. See License.txt in the project root for license information.
+// *----------------------------------------------------------------------------------------------
+#pragma once
+
+#ifndef _YCLFWD_H_
+#define _YCLFWD_H_
+
+#include "code/bit_op.h"
+#include "code/dimension_coord.h"
+
+#include "code/group.h"
+
+#endif // _YCLFWD_H_
